5b752dabf93616cb01a0d18f0adb5a5410faedd8
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-04-14 12:16:52 +0200

    d

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-const btn = document.querySelector('button')
-const display = document.querySelector('main')
-
-btn.addEventListener('click', function(){
-    const btn = document.querySelector('button');
+const btn = document.querySelector('button');
         const display = document.querySelector('main');
         let i=1
-        btn.addEventListener('click', function(){
-            const newElement = document.createElement('p');
-            newElement.style.color = '#f00';
-            newElement.textContent = 'Nowy element'
-            display.append(newElement);
+btn.addEventListener('click', function(){
+    const newElementP = document.createElement('p');
+    newElementP.style.color = '#f00';
+    newElementP.textContent = 'Nowy element'
+    display.append(newElement);
         
 
             const newElementH3 = document.createElement('h3');
             newElementH3.style.color.backgroundColor = '#ff0';
-            const h3TextNode = document.createTextNode('Jakiś tytuł $(i)');
+            const h3TextNode = document.createTextNode('Jakiś tytuł' $(i));
             newElementH3.addEventListener('click', function (evt){
                 console.log(this);
                 this.remove()
             })
             newElementH3.append(h3TextNode);
 
-            display.append(newElementH3, newElement, 'dodany tekst');
+            display.append(newElementH3, newElementP, 'dodany tekst');
             i++;
 })
