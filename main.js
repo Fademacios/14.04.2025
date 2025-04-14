472b0257c65bd30f810ac4eb103de442a512e89f
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-04-14 11:41:29 +0200

    g

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const btn = document.querySelector('button')
+const display = document.querySelector('main')
 
+btn.addEventListener('click', function(){
+    const newelement = document.createElement('p')
+    newelement.style.color = '#f00'
+    newelement.textContent = 'nowy element'
+    display.append(newelement)
+
+})
