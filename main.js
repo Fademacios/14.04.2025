a76735255010872511aa15a98c111cd776a85417
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-04-14 12:01:15 +0200

    u

--- main.js
+++ main.js
@@ -7,4 +7,12 @@ btn.addEventListener('click', function(){
     newelement.textContent = 'nowy element'
     display.append(newelement)
 
+
+    const newElementH3 = document.createElement('h3');
+            newElementH3.style.color.backgroundColor = '#ff0';
+            const h3TextNode = document.createTextNode('Jakiś tytuł');
+
+            newElementH3.append(h3TextNode);
+
+            display.append(newElementH3, newElementP, 'dodany tekst');
 })
